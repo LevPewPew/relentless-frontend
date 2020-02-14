71b8e8d5bec874c0e371b53dcd8f2c5287e1106c
LevPewPew <levthedev@protonmail.com>
2020-02-14 15:43:15 +1100

    basic form with Create functionality for cards collection

--- src/components/CardForm/CardForm.jsx
+++ src/components/CardForm/CardForm.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 import { useForm } from 'react-hook-form';
 import axios from 'axios';
 import { CardSubmitBtn } from '../components';
@@ -6,6 +7,7 @@ import { CardSubmitBtn } from '../components';
 const webServer = process.env.REACT_APP_BACKEND_URL;
 
 function CardForm() {
+  const dispatch = useDispatch();
   const { register, handleSubmit, errors } = useForm();
 
   const onSubmit = (data) => {
@@ -15,26 +17,18 @@ function CardForm() {
       return res;
     }
 
-    // const setCardsData = async () => {
-    //   try {
-    //     let res = await getCardsData();
-    //     dispatch({ type: 'SET_CARDS_DATA', newCardsData: res.data });
-    //   } catch (err) {
-    //     console.log(err);
-    //   }
-    // }
+    const setCardsData = (newCard) => {
+      dispatch({ type: 'APPEND_CARDS_DATA', newCards: [newCard] });
+    }
 
     try {
       postCardsData(data);
+      setCardsData(data);
     } catch (err) {
       console.log(err);
     }
-
-    console.log('DATA: ', data);
   }
 
-  // console.log(errors);
-  
   return (
     <form className="CardForm" onSubmit={handleSubmit(onSubmit)}>
       <textarea name="question" ref={register} rows="10" cols="50" />
--- src/reducers/cardsReducer.js
+++ src/reducers/cardsReducer.js
@@ -9,6 +9,10 @@ function cardsReducer(state = initialState, action) {
     case 'SET_CARDS_DATA':
       newState = { ...state, cardsData: action.newCardsData };
       break;
+    case 'APPEND_CARDS_DATA':
+      let newCardsData = [...state.cardsData, ...action.newCards];
+      newState = { ...state, cardsData: newCardsData };
+      break;
     default:
       newState = { ...state };
   }
